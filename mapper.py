8add8f9e234bf4f0680fe5b3bc1a66d4c35d79d4
poblemasso <96197746+poblemasso@users.noreply.github.com>
2022-02-10 18:45:38 +0100

    Create mapper.py

---
+++ mapper.py
@@ -0,0 +1,21 @@
+import sys
+import io
+import re
+import nltk
+nltk.download('stopwords',quiet=True)
+from nltk.corpus import stopwords
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+stop_words = set(stopwords.words('english'))
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='latin1')
+for line in input_stream:
+  line = line.strip()
+  line = re.sub(r'[^\w\s]', '',line)
+  line = line.lower()
+  for x in line:
+    if x in punctuations:
+      line=line.replace(x, " ") 
+
+  words=line.split()
+  for word in words: 
+    print(word[0])
